e2a80d87103678e22254e7e1f2d79013d5ddae44
dobrijjuk <dobrijjuk@gmail.com>
2017-11-17 01:10:20 +0200

    Hii

--- MyTestGitHubProject/Program.cs
+++ MyTestGitHubProject/Program.cs
@@ -11,6 +11,7 @@ namespace MyTestGitHubProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hiiii");
         }
     }
 }
